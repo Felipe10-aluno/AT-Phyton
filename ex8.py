fdd9f7cdbc49284925e943a5cf79b3f8b20b30d0
Felipe10-aluno <felipe_lima-pereira@estudante.sesisenai.org.br>
2024-08-26 17:12:58 -0300

    Update ex8.py

--- ex8.py
+++ ex8.py
@@ -1,18 +1,20 @@
 def dobrar_lista(lista): 
-  nova_lista = []
-  for elemento in lista:
-    novo_elemento = elemento * 2 
-    nova_lista.append(novo_elemento) 
-    return nova_lista 
+ nova_lista = []
+ for elemento in lista:
+  novo_elemento = elemento * 2
+  nova_lista.append(novo_elemento) 
+ return nova_lista 
 lista=list()
+#aqui ele faz a funcao de dobrar o numero que vai ser digitado
 i=1
 while i<=10:
- elem = int(input("Digite um elemento da lista:")) 
+  
+ elem = int(input("Digite um elemento da lista:"))
  lista.append(elem)
  i+=1 
-print(lista) 
-nova_lista = dobrar_lista(lista)
-print(nova_lista)
+ print(lista) 
+ nova_lista = dobrar_lista(lista)
+ print(nova_lista)
 
 #'1 a 9' o codigo basicamente faz a analise dos 10 elementos da lista para a duplicação da mesma
 #'10 a 15' o codigo recebe os valores e os imprime na tela, para o resultado 
